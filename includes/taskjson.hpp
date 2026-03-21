70d59630b8802b4f011d8b166fa4ed8b5a10d00b
Vince <vincez7878@gmail.com>
2026-03-21 06:15:38 +0000

    made json reading skeleton

---
+++ includes/taskjson.hpp
@@ -0,0 +1,15 @@
+#ifndef TASKJSON_HPP
+#define TASKJSON_HPP
+
+#include <fstream>
+
+#include "json.hpp"
+#include "task.hpp"
+#include "taskmanager.hpp"
+
+using json = nlohmann::json;
+
+//todo
+
+
+#endif
--- includes/taskmanager.hpp
+++ includes/taskmanager.hpp
@@ -7,17 +7,11 @@
 #include <iostream>
 
 void RouteCommands(std::vector<std::string>& commands, std::vector<Task>& tasks);
-
 void AddTask(std::vector<std::string>& commands, std::vector<Task>& tasks);
-
 void UpdateTask(std::vector<std::string>& commands, std::vector<Task>& tasks);
-
 void DeleteTask(std::vector<std::string>& commands, std::vector<Task>& tasks);
-
 void MarkTask(std::vector<std::string>& commands, std::vector<Task>& tasks);
-
 void ListTasks(std::vector<std::string>& commands, std::vector<Task>& tasks);
-
 void ListTasksByStatus(std::vector<std::string>& commands, std::vector<Task>& tasks);
 
 #endif
